aad89de1fa24780a054879bf80eec0c07f5f3156
Shubham Panchal <panchalshubhamdev@gmail.com>
2025-06-17 07:44:16 +0530

    add support for 16KB page size Android devices
    No explicit changes were made to the compilation process; the NDK version was upgraded to r28b; NDKs > r28 compile for 16KB page size targets as a default

--- model2vec/build.gradle.kts
+++ model2vec/build.gradle.kts
@@ -18,7 +18,7 @@ android {
 
     // Declare the ndkVersion to avoid 'NDK not installed' errors from rust-android-plugin
     // see: https://github.com/mozilla/rust-android-gradle/issues/29#issuecomment-593501017
-    ndkVersion = "27.2.12479018" // Android NDK r27c
+    ndkVersion = "28.1.13356709" // Android NDK r28b
     envAndroidNDKPath?.let {
         ndkPath = it
     }
